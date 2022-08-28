71c9a9093f7bc19b78079e6572b725459b9d9a28
jeanfg86 <jeanfg86@gmail.com>
2022-08-27 21:28:41 -0300

    ensure RemoteAuthentication throw UnexpectedError if HttpClient returns 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:survey_flutter/data/http/http.dart';
 import 'package:survey_flutter/data/usecases/usecases.dart';
+import 'package:survey_flutter/domain/helpers/helpers.dart';
 import 'package:survey_flutter/domain/usecases/usecases.dart';
 import 'package:test/test.dart';
 
@@ -27,5 +28,19 @@ void main() {
         url: url,
         method: 'post',
         body: {'email': params.email, 'password': params.secret}));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    When mockRequestCall() => when(() => httpClient.request(
+        url: any(named: 'url'),
+        method: any(named: 'method'),
+        body: any(named: 'body')));
+    mockRequestCall().thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
