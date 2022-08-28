ff66ad30557686787227fef539ba607e59b855ea
jeanfg86 <jeanfg86@gmail.com>
2022-08-28 20:07:16 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,12 +8,14 @@ import 'package:test/test.dart';
 class HttpAdapter {
   final Client client;
   HttpAdapter(this.client);
-  Future<void> request({required String url, required String method}) async {
+  Future<void> request(
+      {required String url, required String method, Map? body}) async {
     late Map<String, String> headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(Uri.parse(url), headers: headers);
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
   }
 }
 
@@ -19,8 +23,8 @@ class ClientSpy extends Mock implements Client {
   ClientSpy() {
     mockPost(200);
   }
-  When mockPostCall() =>
-      when(() => this.post(any(), headers: any(named: 'headers')));
+  When mockPostCall() => when(() => this
+      .post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
   void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
       mockPostCall().thenAnswer((_) async => Response(body, statusCode));
 }
@@ -41,12 +45,21 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct Values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(() => client.post(Uri.parse(url), headers: {
+      verify(() => client.post(Uri.parse(url),
+          headers: {
             'content-type': 'application/json',
             'accept': 'application/json'
-          }));
+          },
+          body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(() => client.post(any(), headers: any(named: 'headers')));
     });
   });
 }
