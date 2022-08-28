0819166b109bea92e516a7a960f4dbe15e5b0a9d
jeanfg86 <jeanfg86@gmail.com>
2022-08-28 19:48:39 -0300

    ensure HttpAdapter calls post with correct URL

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,44 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class HttpAdapter {
+  final Client client;
+  HttpAdapter(this.client);
+  Future<void> request({required String url}) async {
+    await client.post(Uri.parse(url));
+  }
+}
+
+class ClientSpy extends Mock implements Client {
+  ClientSpy() {
+    mockPost(200);
+  }
+  When mockPostCall() => when(() => this.post(any()));
+  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+}
+
+void main() {
+  late HttpAdapter sut;
+  late ClientSpy client;
+  late String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
+  });
+  group('post', () {
+    test('Should call post with correct Values', () async {
+      await sut.request(url: url);
+
+      verify(() => client.post(Uri.parse(url)));
+    });
+  });
+}
