576369cc2a939c8e8bb5aef9fad1e5ccbd45bf09
jeanfg86 <jeanfg86@gmail.com>
2022-08-30 22:17:37 -0300

    create style page

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -5,9 +5,22 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color.fromARGB(255, 50, 57, 154);
+    final primaryColorDark = Color.fromARGB(255, 3, 8, 71);
+    final primaryColorLight = Color.fromARGB(255, 97, 161, 218);
+    final ThemeData theme = ThemeData();
+
     return MaterialApp(
       title: 'Survey',
       debugShowCheckedModeBanner: false,
+      theme: theme.copyWith(
+          primaryColor: primaryColor,
+          primaryColorDark: primaryColorDark,
+          primaryColorLight: primaryColorLight,
+          colorScheme: theme.colorScheme.copyWith(secondary: primaryColor),
+          backgroundColor: Colors.white,
+          textTheme:
+              TextTheme(headline1: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: primaryColorDark))),
       home: LoginPage(),
     );
   }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -7,11 +7,25 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           Container(
+            height: 240,
+            margin: EdgeInsets.only(bottom: 32),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [Theme.of(context).primaryColorLight, Theme.of(context).primaryColorDark]),
+                boxShadow: [BoxShadow(offset: Offset(0, 0), spreadRadius: 0, blurRadius: 4, color: Colors.black)],
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.elliptical(20, 20), bottomRight: Radius.elliptical(20, 20))),
             child: Image(image: AssetImage('lib/ui/assets/logo.png')),
           ),
-          Text('Login'.toUpperCase()),
+          Text(
+            'Login'.toUpperCase(),
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
           Form(
               child: Column(
             children: [
