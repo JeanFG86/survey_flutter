1605b5340519f9525d156c7ab01db455802f4d57
jeanfg86 <jeanfg86@gmail.com>
2022-09-07 19:43:36 -0300

    move RequiredFieldValidation to its own file

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -1,20 +1,5 @@
+import 'package:survey_flutter/validation/validators/validators.dart';
 import 'package:test/test.dart';
-
-abstract class FieldValidation {
-  String get field;
-  String? validate(String? value);
-}
-
-class RequiredFieldValidation implements FieldValidation {
-  @override
-  final String field;
-  RequiredFieldValidation(this.field);
-
-  @override
-  String? validate(String? value) {
-    return value?.isNotEmpty == true ? null : 'Campo obrigatório';
-  }
-}
 
 void main() {
   late RequiredFieldValidation sut;
