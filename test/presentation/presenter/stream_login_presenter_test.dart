cfbe7840540aca4f4b94febad1ac9fe79adf4f51
jeanfg86 <jeanfg86@gmail.com>
2022-09-07 15:36:16 -0300

    ensure IsFormValid emits null if validation succeeds

--- test/presentation/presenter/stream_login_presenter_test.dart
+++ test/presentation/presenter/stream_login_presenter_test.dart
@@ -43,5 +43,12 @@ void main() {
     sut.validateEmail(email);
     sut.validateEmail(email);
   });
+
+  test('Should emit null if validation succeeds', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
 }
-//continua 8min50
