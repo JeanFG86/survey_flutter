9dc72cbf24fa2442b1956d24242881f8f679c94b
jeanfg86 <jeanfg86@gmail.com>
2022-09-07 19:28:22 -0300

    ensure RequiredFieldValidation returns error if value is null

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -2,33 +2,35 @@ import 'package:test/test.dart';
 
 abstract class FieldValidation {
   String get field;
-  String? validate(String value);
+  String? validate(String? value);
 }
 
 class RequiredFieldValidation implements FieldValidation {
+  @override
   final String field;
   RequiredFieldValidation(this.field);
 
   @override
-  String? validate(String value) {
-    return value.isEmpty ? 'Campo obrigatório' : null;
+  String? validate(String? value) {
+    return value?.isNotEmpty == true ? null : 'Campo obrigatório';
   }
 }
 
 void main() {
+  late RequiredFieldValidation sut;
+
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
   test('Should return null if value is not empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
-    final error = sut.validate('any_value');
-
-    expect(error, null);
+    expect(sut.validate('any_value'), null);
   });
 
   test('Should return error if value is empty', () {
-    final sut = RequiredFieldValidation('any_field');
+    expect(sut.validate(''), 'Campo obrigatório');
+  });
 
-    final error = sut.validate('');
-
-    expect(error, 'Campo obrigatório');
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), 'Campo obrigatório');
   });
 }
