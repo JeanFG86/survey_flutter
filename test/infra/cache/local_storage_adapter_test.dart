1b32786b6be0a67771bd4f551f396f2ae4d2df42
jeanfg86 <jeanfg86@gmail.com>
2022-09-11 21:11:06 -0300

    move SUT creation to the setup method

---
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -0,0 +1,53 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:survey_flutter/data/cache/cache.dart';
+import 'package:test/test.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+
+  LocalStorageAdapter({required this.secureStorage});
+
+  @override
+  Future<void> saveSecure({required String key, required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
+
+class FlutterSecurityStorageSpy extends Mock implements FlutterSecureStorage {
+  FlutterSecurityStorageSpy() {
+    mockSave();
+  }
+
+  When mockSaveCall() => when(() => write(key: any(named: 'key'), value: any(named: 'value')));
+  void mockSave() => mockSaveCall().thenAnswer((_) async => _);
+  void mockSaveError() => when(() => mockSaveCall().thenThrow(Exception()));
+
+  When mockFetchCall() => when(() => read(key: any(named: 'key')));
+  void mockFetch(String? data) => mockFetchCall().thenAnswer((_) async => data);
+  void mockFetchError() => when(() => mockFetchCall().thenThrow(Exception()));
+}
+
+void main() {
+  late LocalStorageAdapter sut;
+  late FlutterSecurityStorageSpy localStorage;
+  late String key;
+  late dynamic value;
+  late String result;
+
+  setUp(() {
+    key = faker.randomGenerator.string(5);
+    value = faker.randomGenerator.string(50);
+    result = faker.randomGenerator.string(50);
+    localStorage = FlutterSecurityStorageSpy();
+    localStorage.mockFetch(key);
+    sut = LocalStorageAdapter(secureStorage: localStorage);
+  });
+
+  test('Should call save secure with correct values', () async {
+    await sut.saveSecure(key: key, value: value);
+
+    verify(() => localStorage.write(key: key, value: value));
+  });
+}
