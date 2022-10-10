857bbbfd6fcc5eb8a63774ca236e88cb53b2e4ca
jeanfg86 <jeanfg86@gmail.com>
2022-10-10 20:40:23 -0300

    add surveyEntity

---
+++ lib/domain/entities/survey_entity.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SurveyEntity {
+  final String id;
+  final String question;
+  final DateTime dateTime;
+  final bool didAnswer;
+
+  const SurveyEntity({required this.id, required this.question, required this.dateTime, required this.didAnswer});
+}
