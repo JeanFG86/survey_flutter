a0f39d9884173aa2752608f24fba0274ee13c138
jeanfg86 <jeanfg86@gmail.com>
2022-10-14 19:48:55 -0300

    ensure RemoteLoadSurveys throws UnexpectedError if HttpClient returns 500

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -104,4 +104,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    httpClient.mockRequestError(HttpError.serverError);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
