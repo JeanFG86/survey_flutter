7e25cf8004386f680904f2bd9e98856b2589a824
jeanfg86 <jeanfg86@gmail.com>
2022-10-13 21:56:51 -0300

    ensure RemoteLoadSurveys returns surveys on 200

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -1,6 +1,8 @@
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:survey_flutter/data/http/http.dart';
+import 'package:survey_flutter/data/models/models.dart';
+import 'package:survey_flutter/domain/entities/entities.dart';
 import 'package:test/test.dart';
 
 class RemoteLoadSurveys {
@@ -9,21 +11,74 @@ class RemoteLoadSurveys {
 
   RemoteLoadSurveys({required this.url, required this.httpClient});
 
-  Future<void> load() async {
-    await httpClient.request(url: url, method: 'get');
+  Future<List<SurveyEntity>> load() async {
+    final httpResponse = await httpClient.request(url: url, method: 'get');
+    return httpResponse.map<SurveyEntity>((json) => RemoteSurveyModel.fromJson(json).toEntity()).toList();
   }
 }
 
-class HttpClientSpy extends Mock implements HttpClient {}
+class HttpClientSpy extends Mock implements HttpClient {
+  When mockRequestCall() =>
+      when(() => request(url: any(named: 'url'), method: any(named: 'method'), body: any(named: 'body')));
+  void mockRequest(dynamic data) => mockRequestCall().thenAnswer((_) async => data);
+  void mockRequestError(HttpError error) => mockRequestCall().thenThrow(error);
+}
+
+class ApiFactory {
+  static Map makeAccountJson() => {'accessToken': faker.guid.guid(), 'name': faker.person.name()};
+
+  static Map makeSurveyResultJson() => {
+        'surveyId': faker.guid.guid(),
+        'question': faker.randomGenerator.string(50),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+          {
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          }
+        ],
+        'date': faker.date.dateTime().toIso8601String(),
+      };
+
+  static List<Map> makeSurveyList() => [
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(50),
+          'didAnswer': faker.randomGenerator.boolean(),
+          'date': faker.date.dateTime().toIso8601String(),
+        },
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(50),
+          'didAnswer': faker.randomGenerator.boolean(),
+          'date': faker.date.dateTime().toIso8601String(),
+        }
+      ];
+
+  static Map makeInvalidJson() => {'invalid_key': 'invalid_value'};
+
+  static List<Map> makeInvalidList() => [makeInvalidJson(), makeInvalidJson()];
+}
 
 void main() {
   late RemoteLoadSurveys sut;
   late HttpClientSpy httpClient;
   late String url;
+  late List<Map> list;
 
   setUp(() {
+    list = ApiFactory.makeSurveyList();
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
+    httpClient.mockRequest(list);
     sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
   });
 
@@ -31,5 +86,24 @@ void main() {
     await sut.load();
 
     verify(() => httpClient.request(url: url, method: 'get'));
+  });
+
+  test('Should return surveys on 200', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, [
+      SurveyEntity(
+        id: list[0]['id'],
+        question: list[0]['question'],
+        dateTime: DateTime.parse(list[0]['date']),
+        didAnswer: list[0]['didAnswer'],
+      ),
+      SurveyEntity(
+        id: list[1]['id'],
+        question: list[1]['question'],
+        dateTime: DateTime.parse(list[1]['date']),
+        didAnswer: list[1]['didAnswer'],
+      )
+    ]);
   });
 }
