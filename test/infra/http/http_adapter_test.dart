f657258df4f5e1919b56c880eda7f1ce8fa3129f
jeanfg86 <jeanfg86@gmail.com>
2022-10-15 20:37:23 -0300

    Ensure HttpAdapter return ServerError if get returns 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -221,5 +221,13 @@ void main() {
 
       expect(future, throwsA(HttpError.notFound));
     });
+
+    test('Should return ServerError if get returns 500', () async {
+      client.mockGet(500);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
