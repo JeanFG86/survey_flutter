cd56913f8497be0d75fac82977ceeef912452f2f
jeanfg86 <jeanfg86@gmail.com>
2022-10-15 20:37:00 -0300

    Ensure HttpAdapter return NotFoundError if get returns 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -213,5 +213,13 @@ void main() {
 
       expect(future, throwsA(HttpError.forbidden));
     });
+
+    test('Should return NotFoundError if get returns 404', () async {
+      client.mockGet(404);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
   });
 }
