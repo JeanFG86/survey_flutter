5fac5de0d2e1d32fb1ebe6f0a7e05e8208e5c9a9
jeanfg86 <jeanfg86@gmail.com>
2022-10-25 20:12:50 -0300

    add Surveys compositions

---
+++ lib/main/factories/pages/surveys/surveys_presenter_factory.dart
@@ -0,0 +1,7 @@
+import '../../../../presentation/presenter/presenter.dart';
+import '../../../../ui/pages/pages.dart';
+import '../../factories.dart';
+
+SurveysPresenter makeGetxSurveysPresenter() {
+  return GetxSurveysPresenter(loadSurveys: makeRemoteLoadSurveys());
+}
--- lib/presentation/presenter/getx_surveys_presenter.dart
+++ lib/presentation/presenter/getx_surveys_presenter.dart
@@ -6,18 +6,21 @@ import '../../domain/usecases/usecases.dart';
 import '../../ui/helpers/errors/errors.dart';
 import '../../ui/pages/pages.dart';
 
-class GetxSurveysPresenter extends GetxController {
+class GetxSurveysPresenter extends GetxController implements SurveysPresenter {
   final LoadSurveys loadSurveys;
   final _surveys = Rx<List<SurveyViewModel>>([]);
   final _isLoading = false.obs;
 
+  @override
   Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
 
+  @override
   Stream<bool> get isLoadingStream => _isLoading.stream;
   set isLoading(bool value) => _isLoading.value = value;
 
   GetxSurveysPresenter({required this.loadSurveys});
 
+  @override
   Future<void> loadData() async {
     try {
       isLoading = true;
