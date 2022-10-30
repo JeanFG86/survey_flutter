e14176d66703ca9fd78906c2b4677b04d8babde8
jeanfg86 <jeanfg86@gmail.com>
2022-10-30 17:12:29 -0300

    move AuthorizeHttpClientDecorator to its own file

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -2,32 +2,8 @@ import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:survey_flutter/data/cache/cache.dart';
 import 'package:survey_flutter/data/http/http.dart';
+import 'package:survey_flutter/main/decorators/decorators.dart';
 import 'package:test/test.dart';
-
-class AuthorizeHttpClientDecorator {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-  final HttpClient decoratee;
-
-  AuthorizeHttpClientDecorator({required this.fetchSecureCacheStorage, required this.decoratee});
-
-  Future<dynamic> request({
-    required String url,
-    required String method,
-    Map? body,
-    Map? headers,
-  }) async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure('token');
-      final authorizedHeaders = headers ?? {}
-        ..addAll({'x-access-token': token});
-      return await decoratee.request(url: url, method: method, body: body, headers: authorizedHeaders);
-    } on HttpError {
-      rethrow;
-    } catch (error) {
-      throw HttpError.forbidden;
-    }
-  }
-}
 
 class SecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {
   SecureCacheStorageSpy();
