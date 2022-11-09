f4ff3007d888d2565e1425681b21e364738231bc
jeanfg86 <jeanfg86@gmail.com>
2022-11-09 20:19:07 -0300

    ensure LocalStorageAdapter throw if deleteItem throws

--- test/infra/cache/local_storage_adpter_test.dart
+++ test/infra/cache/local_storage_adpter_test.dart
@@ -70,5 +70,13 @@ void main() {
 
       verify(() => localStorage.deleteItem(key)).called(1);
     });
+
+    test('Should throw if deleteItem throws', () async {
+      localStorage.mockDeleteError();
+
+      final future = sut.delete(key);
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
   });
 }
