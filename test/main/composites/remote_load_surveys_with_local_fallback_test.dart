57fdfc63433af8af09a90bb520c3426f5605a0cd
jeanfg86 <jeanfg86@gmail.com>
2022-11-12 19:40:54 -0300

    ensure Composite calls local save with remote data

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,34 +1,87 @@
+import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:survey_flutter/data/usecases/usecases.dart';
 import 'package:survey_flutter/domain/entities/entities.dart';
+import 'package:survey_flutter/domain/helpers/helpers.dart';
 import 'package:test/test.dart';
 
 class RemoteLoadSurveysWithLocalFallback {
   final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
 
-  RemoteLoadSurveysWithLocalFallback({required this.remote});
+  RemoteLoadSurveysWithLocalFallback({required this.remote, required this.local});
 
   Future<void> load() async {
-    await remote.load();
+    final surveys = await remote.load();
+    await local.save(surveys);
   }
 }
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {
   When mockLoadCall() => when(() => load());
   void mockLoad(List<SurveyEntity> surveys) => mockLoadCall().thenAnswer((_) async => surveys);
+  void mockLoadError(DomainError error) => mockLoadCall().thenThrow(error);
+}
+
+class LocalLoadSurveysSpy extends Mock implements LocalLoadSurveys {
+  LocalLoadSurveysSpy() {
+    mockValidate();
+    mockSave();
+  }
+  When mockLoadCall() => when(() => load());
+  void mockLoad(List<SurveyEntity> surveys) => mockLoadCall().thenAnswer((_) async => surveys);
+  void mockLoadError() => mockLoadCall().thenThrow(DomainError.unexpected);
+
+  When mockValidateCall() => when(() => validate());
+  void mockValidate() => mockValidateCall().thenAnswer((_) async => _);
+  void mockValidateError() => mockValidateCall().thenThrow(Exception());
+
+  When mockSaveCall() => when(() => save(any()));
+  void mockSave() => mockSaveCall().thenAnswer((_) async => _);
+  void mockSaveError() => mockSaveCall().thenThrow(Exception());
+}
+
+class EntityFactory {
+  static List<SurveyEntity> makeSurveyList() => [
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.randomGenerator.string(10),
+            dateTime: DateTime.utc(2020, 2, 2),
+            didAnswer: true),
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.randomGenerator.string(10),
+            dateTime: DateTime.utc(2018, 12, 20),
+            didAnswer: false)
+      ];
 }
 
 void main() {
   late RemoteLoadSurveysWithLocalFallback sut;
   late RemoteLoadSurveysSpy remote;
+  late LocalLoadSurveysSpy local;
+  late List<SurveyEntity> remoteSurveys;
+  late List<SurveyEntity> localSurveys;
 
   setUp(() {
+    localSurveys = EntityFactory.makeSurveyList();
+    local = LocalLoadSurveysSpy();
+    local.mockLoad(localSurveys);
+    remoteSurveys = EntityFactory.makeSurveyList();
     remote = RemoteLoadSurveysSpy();
-    sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+    remote.mockLoad(remoteSurveys);
+    sut = RemoteLoadSurveysWithLocalFallback(remote: remote, local: local);
   });
+
   test('Should call remote load', () async {
     await sut.load();
 
     verify(() => remote.load()).called(1);
+  });
+
+  test('Should call local save with remote data', () async {
+    await sut.load();
+
+    verify(() => local.save(remoteSurveys)).called(1);
   });
 }
