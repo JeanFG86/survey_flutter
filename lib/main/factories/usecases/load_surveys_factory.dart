9c7c098c8a4c013d1578567fe4f044904d3e9811
jeanfg86 <jeanfg86@gmail.com>
2022-11-13 21:09:32 -0300

    add RemoteLoadSurveysWithLocalFallback factory

--- lib/main/factories/usecases/load_surveys_factory.dart
+++ lib/main/factories/usecases/load_surveys_factory.dart
@@ -1,7 +1,13 @@
 import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
+import '../../composites/composites.dart';
+import '../cache/cache.dart';
 import '../factories.dart';
 
-LoadSurveys makeRemoteLoadSurveys() {
-  return RemoteLoadSurveys(httpClient: makeAuthorizeHttpClientDecorator(), url: makeApiUrl('surveys'));
-}
+RemoteLoadSurveys makeRemoteLoadSurveys() =>
+    RemoteLoadSurveys(httpClient: makeAuthorizeHttpClientDecorator(), url: makeApiUrl('surveys'));
+
+LocalLoadSurveys makeLocalLoadSurveys() => LocalLoadSurveys(cacheStorage: makeLocalStorageAdapter());
+
+LoadSurveys makeRemoteLoadSurveysWithLocalFallback() =>
+    RemoteLoadSurveysWithLocalFallback(remote: makeRemoteLoadSurveys(), local: makeLocalLoadSurveys());
