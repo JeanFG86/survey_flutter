0c16268157015b1cfbeab0422066b0324228c775
jeanfg86 <jeanfg86@gmail.com>
2022-11-15 12:08:41 -0300

    move ReloadScreen Component to iots own file

---
+++ lib/ui/components/reload_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/i18n/i18n.dart';
+
+class ReloadScreen extends StatelessWidget {
+  final String error;
+  final Future<void> Function() reload;
+
+  const ReloadScreen({
+    required this.error,
+    required this.reload,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            error,
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: reload, child: Text(R.string.reload))
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -28,22 +28,9 @@ class SurveysPage extends StatelessWidget {
             stream: presenter.surveysStream,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '${snapshot.error}',
-                        style: const TextStyle(fontSize: 16),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      ElevatedButton(onPressed: presenter.loadData, child: Text(R.string.reload))
-                    ],
-                  ),
+                return ReloadScreen(
+                  error: snapshot.error.toString(),
+                  reload: presenter.loadData,
                 );
               }
               if (snapshot.hasData) {
@@ -61,5 +48,7 @@ class SurveysPage extends StatelessWidget {
     );
   }
 }
+
+
 
 //items: snapshot.data?.map((viewModel) => SurveyItem(viewModel)).toList(),
